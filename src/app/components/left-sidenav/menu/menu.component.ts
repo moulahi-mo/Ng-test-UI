f86097152afacbf701167a1b403e086985bf4377
moulahi-mo <moulahi.mo@gmail.com>
2021-03-16 15:03:12 +0100

    fixing reactive selection for the menu links

--- src/app/components/left-sidenav/menu/menu.component.ts
+++ src/app/components/left-sidenav/menu/menu.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Menu {
+  link: string;
+  isClicked: boolean;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  menu: Menu[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.menu = [
+      { link: 'home', isClicked: true },
+      { link: 'messages', isClicked: false },
+      { link: 'profile', isClicked: false },
+      { link: 'saved post', isClicked: false },
+      { link: 'settings', isClicked: false },
+    ];
   }
-
+  // ! function that's allow:  exact selection of one link at time by adding active class
+  public VerifyClicks(i: number) {
+    this.menu.forEach((item, index) => {
+      index == i ? (item.isClicked = true) : (item.isClicked = false);
+    });
+  }
 }
